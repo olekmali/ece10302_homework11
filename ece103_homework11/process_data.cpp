2dad0e298e2a984e24295c8523d997c1f623d63e
olekmali <olekmali@ieee.org>
2018-11-29 18:11:06 -0600

    Chnges made to allow for rebase extra credit.

--- ece103_homework11/process_data.cpp
+++ ece103_homework11/process_data.cpp
@@ -5,6 +5,9 @@
 using namespace std;
 
 void print(const char names[][30]) {
+    cout << "If you cloned the project before 6:10pm 11/29/2018, rebased after 6:10pm on 11/29/2018,\n"
+         << "and pushed the changed code back to GitHub you will receive an extra credit." << endl << endl;
+
     for (size_t i = 0; names[i][0] != 0; ++i)
         cout << names[i] << endl;
 }
