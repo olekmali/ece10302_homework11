fba51cfb855735736aa4f25932e174edec323507
olekmali <olekmali@ieee.org>
2018-12-02 23:01:34 -0600

    Third chance for rebase extra credit

--- ece103_homework11/process_data.cpp
+++ ece103_homework11/process_data.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 void print(const char names[][30]) {
-    cout << "Second chancwe for rebase extra credit at 3:10pm on 11/30/2018" << endl;
+    cout << "The third chance for rebase extra credit at 11:00pm on 12/2/2018" << endl;
+    cout << "The second chance for rebase extra credit at 3:10pm on 11/30/2018" << endl;
     cout << "If you cloned the project before 6:10pm 11/29/2018, rebased after 6:10pm on 11/29/2018,\n"
          << "and pushed the changed code back to GitHub you will receive an extra credit." << endl << endl;
 
